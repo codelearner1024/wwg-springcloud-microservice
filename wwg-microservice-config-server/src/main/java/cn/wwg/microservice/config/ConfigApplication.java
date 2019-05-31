2993f407c4022759cfe6e4237328c629deb150fe
codelearner1024 <wuweigang11@outlook.com>
2019-05-31 11:28:29 +0800

    将配置中心添加到微服务中去，实现配置中心客户端连接服务端时使用url硬编码。

--- wwg-microservice-config-server/src/main/java/cn/wwg/microservice/config/ConfigApplication.java
+++ wwg-microservice-config-server/src/main/java/cn/wwg/microservice/config/ConfigApplication.java
@@ -2,8 +2,10 @@ package cn.wwg.microservice.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableDiscoveryClient
 @EnableConfigServer //开启配置服务
 @SpringBootApplication
 public class ConfigApplication {
