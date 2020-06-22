ac6088fa411f5fce1ad47ae245e1c4ce8f5b7c20
wuwei <wuweigang11@outlook.com>
2020-06-22 11:21:55 +0800

    增加zipkin链路监控模块，及相关客户端接入配置

---
+++ w-zipkin-server/src/main/java/cn/wwg/microservice/zipkin/ZipkinApplication.java
@@ -0,0 +1,18 @@
+package cn.wwg.microservice.zipkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+import zipkin.server.EnableZipkinServer;
+
+@EnableZipkinServer
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ZipkinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZipkinApplication.class, args);
+	}
+
+}
